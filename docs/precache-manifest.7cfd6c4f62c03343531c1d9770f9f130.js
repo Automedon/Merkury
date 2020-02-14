40806068b1e807d4088b0c88120f8c66b936fd7e
Automedon <42299107+Automedon@users.noreply.github.com>
2020-02-14 21:00:56 +0700

    PSD Merkury

---
+++ docs/precache-manifest.7cfd6c4f62c03343531c1d9770f9f130.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c399a194f887e501daf42e33a6574d44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62ef8a354f9bc4d507df",
+    "url": "/static/css/2.0ee74ed5.chunk.css"
+  },
+  {
+    "revision": "53e8fa6e8a2d0c00ad59",
+    "url": "/static/css/main.92add6cb.chunk.css"
+  },
+  {
+    "revision": "62ef8a354f9bc4d507df",
+    "url": "/static/js/2.f22dfac1.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.f22dfac1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53e8fa6e8a2d0c00ad59",
+    "url": "/static/js/main.a11e259f.chunk.js"
+  },
+  {
+    "revision": "36d767d8facb351bd990",
+    "url": "/static/js/runtime-main.8f149859.js"
+  },
+  {
+    "revision": "66fd68b9a6f23f8e85ed7205e394238f",
+    "url": "/static/media/Dashboard.66fd68b9.png"
+  },
+  {
+    "revision": "9fcf9a67300e2a87a7559e6182359373",
+    "url": "/static/media/DnD.9fcf9a67.png"
+  },
+  {
+    "revision": "45ccbd38c57c360c90839fab9df8e902",
+    "url": "/static/media/Easy to customize.45ccbd38.png"
+  },
+  {
+    "revision": "ae11e50e5554858b2747dcbf7f6e5bf2",
+    "url": "/static/media/Grupa 2.ae11e50e.png"
+  },
+  {
+    "revision": "222924ea0294eb2618d32145731cde2a",
+    "url": "/static/media/HeaderImg.222924ea.png"
+  },
+  {
+    "revision": "128ecf77b9c6296e2842d2c47ddeee85",
+    "url": "/static/media/Section3Bg.128ecf77.png"
+  }
+]);
